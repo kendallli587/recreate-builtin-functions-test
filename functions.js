834982801d7d11eebf9b70d643cbbb0afe93c680
kendallli587 <kendallli587@gmail.com>
2022-07-07 10:00:05 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,19 +1,10 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
 
 function includes(item, array) {
-    // console.log(JSON.stringify(array[2]))
-
-    // console.log(JSON.stringify(item))
-    // console.log(JSON.stringify(array[2]) === JSON.stringify(item))
-
-    let arrayStr = JSON.stringify(array);
-    let itemStr = JSON.stringify(item);
-
     for (let i = 0; i < array.length; i++) {
-        if (arrayStr[i] === itemStr)  {
+        if (array[i] === item)  {
             return true;
-        } else {
-            return false;
         }
     }
+    return false;
 }
